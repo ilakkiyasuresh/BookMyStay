ecdd1c0bee3f9fbaf491dc8bc8e89656dd163488
ilakkiya <ilakkiya.sureshe@gmail.com>
2026-03-24 14:33:08 +0530

    [ilakkiya] added [UC1-WelcomePage]

---
+++ App/src/BookMyStay.java
@@ -0,0 +1,13 @@
+
+public class BookMyStay {
+
+    public static void main(String[] args) {
+
+        // Display welcome message
+        System.out.println("======================================");
+        System.out.println("   Welcome to Hotel Booking System   ");
+        System.out.println("======================================");
+
+        // Application ends here
+    }
+}
